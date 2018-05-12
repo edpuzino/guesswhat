19917bc26120d75e3111be346e55202f573a8bb2
edpuzino <edpuzino@yahoo.com>
2018-05-12 13:21:30 -0700

    branch for lab 4

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var userPoints = 0;
+var favoriteNumber = 143;
+var attempts = 1;
+var guesses = 1;
 //A guessing game to familiarize the user with myself, Edward Puzino
 //A total of five yes or no questions
 
@@ -11,8 +14,8 @@ if (confirm(
     'Welcome to my quessing game!  My name is Edward Puzino.  Would you like to play?'))
 {
     var user = prompt('What is your name?');
-    
-    alert('Hi ' + user + ', I am going to ask you a few questions to see if you know anything about me.  You will be given one point for every correct answer.  Good luck!');
+/*   
+    alert('Hi ' + user + ', I am going to ask you a few questions to help you learn some interesting facts about me.  You will be given one point for every correct answer.  Good luck!');
 
     var marriages = prompt('Have I been married twice?').toLowerCase();
     console.log(user + ' answered ' + marriages + ' to question number 1.');
@@ -61,13 +64,53 @@ if (confirm(
         userPoints++
     } else {
         alert('Sorry, I have only been to Canada and Mexico and neither one with the military');
-    }
-    if (userPoints < 3){
-        alert('You only received ' + userPoints + ' points out of 5, better luck next time ' + user + '.');
-    } else if(userPoints < 5){
-        alert('Not to shabby ' + user + ', you got ' + userPoints + ' out of 5.');
+    }  
+    
+    var chosenNumber = prompt('I have a favorite number between 1 and 200, you have four chances to guess it, please enter your first guess below.')
+        while ((chosenNumber != favoriteNumber) && (attempts < 4 )) {
+            console.log(user + ' has guessed ' + chosenNumber + ' for guess #' + attempts);
+            if (chosenNumber < favoriteNumber){
+                var chosenNumber = prompt('Sorry you did not guess correctly, your guess was too low, that was attempt #' + attempts + ' please try again.');
+            } else {
+                var chosenNumber = prompt('Sorry you did not guess correctly, your guess was too high, that was attempt #' + attempts + ' please try again.');
+            }         
+            attempts++                   
+        }       
+    console.log(user + ' has guessed ' + chosenNumber + ' for guess #' + attempts);    
+    if (chosenNumber == favoriteNumber) {
+        alert('Congratulations, you have guessed correctly, very impresive.');
+        userPoints++
     } else {
-        alert('Amazing job ' + user + ', you got all 5 questions correct. Have we met before?');
+        alert('You were unable to guess my favorite number, it is ' + favoriteNumber);
+    }*/
+
+    var i = 0;
+    var states =['new jersey', 'arizona', 'colorado', 'arkansas'];
+    var chosenState = prompt('I have lived in four states besides Washington, can you guess any of them?  I\'ll give you seven tries this time.').toLowerCase();
+        while ((guesses < 7) && (chosenState != states[i])) {  
+            console.log(user +' has guessed ' + chosenState + ' for guess #' + guesses);    
+            while ((chosenState != states[i]) && (i < states.length)) {            
+            i++ 
+            }
+            guesses++
+        i = 0;       
+        }
+    console.log(user + ' has guessed ' + chosenState + ' for guess #' + guesses);
+    if (chosenState == states[i]) {
+        alert('Great job, you got that right, besides Washington, I have lived in New Jersey, Arizona, Colorado, and Arkansas.');
+        userPoints++
+    } else {
+        alert('Sorry, besides Washington, I have lived in New Jersey, Arizona, Colorado, and Arkansas');
+    }
+
+    if (userPoints < 3){
+        alert('You only received ' + userPoints + ' points out of 7, better luck next time ' + user + '.');
+    } else if(userPoints < 5){
+        alert('Not to shabby ' + user + ', you got ' + userPoints + ' out of 7.');
+    } else if(userPoints < 7){
+        alert('Nice ' + user + ', you got ' + userPoints + ' out of 7, good job!');
+    } else {        
+        alert('Amazing job ' + user + ', you got all 7 questions correct. Have we met before?');
     }
 
 } else {
